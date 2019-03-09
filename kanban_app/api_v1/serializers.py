1b764922770ef50512a9dcd2e7d8e653d222b616
Ermek K <kadyrkulove@gmail.com>
2019-03-09 15:00:30 +0600

    bugs found and fix bugs

---
+++ kanban_app/api_v1/serializers.py
@@ -0,0 +1,9 @@
+from webapp.models import Task
+from rest_framework import serializers
+
+class TaskSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:task-detail')
+
+    class Meta:
+        model = Task
+        fields = ('url', 'id', 'summary', 'description', 'last_edited', 'status', 'time_planned')
--- kanban_app/api_v1/views.py
+++ kanban_app/api_v1/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from webapp.models import Task
+from rest_framework import viewsets
+from api_v1.serializers import TaskSerializer
 
-# Create your views here.
+class NoAuthModelViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+
+
+class TaskViewSet(NoAuthModelViewSet):
+    queryset = Task.objects.all().order_by('status', '-last_edited')
+    serializer_class = TaskSerializer
