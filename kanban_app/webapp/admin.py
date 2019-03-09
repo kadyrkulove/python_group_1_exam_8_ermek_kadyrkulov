57baefdd70b3c35a78dd221a9d42fc4c0dbea749
Ermek K <kadyrkulove@gmail.com>
2019-03-09 13:29:57 +0600

    make migrate webapp

--- kanban_app/webapp/admin.py
+++ kanban_app/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'last_edited']
+    ordering = ['-last_edited']
+    search_fields = ['summary', 'id']
+
+admin.site.register(Task, TaskAdmin)
